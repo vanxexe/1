41998e9dd7ea99b700854ee0f744d40aad755968
vanxexe <iv.lozivskyi@gmail.com>
2026-01-21 07:47:21 +0000

    21.01.2026

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{   
+    std::cout <<"Ivan" <<std::endl;
+    std::cout <<"Lozivskyi" <<std::endl;
+    std::cout <<"-----------------------------" <<std::endl;
+    std::cout <<"Student Card" <<std::endl;
+    std::cout <<"-----------------------------" <<std::endl;
+    std::cout <<"Last name:\tLozivskyi" <<std::endl;
+    std::cout <<"First name:\tIvan" <<std::endl;
+    std::cout <<"Name group:\t P23" <<std::endl;
+    std::cout <<"Name specialty:\t programming" <<std::endl;
+    std::cout <<"-----------------------------" <<std::endl;
+    std::cout <<"_Spring has come, the black earth_" <<std::endl;
+    std::cout <<"_Woke up from its sleep,_" <<std::endl;
+    std::cout <<"_Flowered it with a cornflower,_" <<std::endl;
+    std::cout <<"_Covered it with periwinkle;_" <<std::endl;
+    std::cout <<"_And in the field a lark,_" <<std::endl;
+    std::cout <<"_A nightingale in the grove_" <<std::endl;
+    std::cout <<"_The earth, adorned with spring,_" <<std::endl;
+    std::cout <<"_In the morning they meet.._" <<std::endl;
+    std::cout <<"\tExcerpt from the poem \"Gaydamaki\"" <<std::endl;
+    std::cout <<"-----------------------------" <<std::endl;
+    std::cout <<"   *" <<std::endl;
+    std::cout <<"  * *" <<std::endl;
+    std::cout <<" * * *" <<std::endl;
+    std::cout <<"* * * *" <<std::endl;
+    std::cout <<"   #" <<std::endl;
+    std::cout <<"-----------------------------" <<std::endl;
+    std::cout <<"File path: \"C:\\Games\\MyGame\"" <<std::endl;
+    std::cout <<"Point 1 \tPoint 2 \tPoint 3" <<std::endl;
+    return 0;
+}
